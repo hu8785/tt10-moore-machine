3589f194898d52141f32ee8a710add138885e94a
hu8785 <patilaku96@gmail.com>
2026-02-04 17:30:13 -0500

    Implement Moore machine Verilog code from lab manual

--- src/tt_um_Akanksha_hu8785_moore.v
+++ src/tt_um_Akanksha_hu8785_moore.v
@@ -1,11 +1,6 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
-module tt_Akanksha_hu8785_moore (
+module tt_um_Akanksha_hu8785_moore (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +11,66 @@ module tt_Akanksha_hu8785_moore (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    wire x1 = ui_in[0];
+    wire z1;
+    reg [1:3] y;
+    reg [1:3] next_state;
+    
+    parameter state_a = 3'b000,
+              state_b = 3'b010,
+              state_c = 3'b110,
+              state_d = 3'b100,
+              state_e = 3'b011;
+    
+    always@(posedge clk) begin
+        if(rst_n)
+            y = state_a;
+        else
+            y = next_state;
+    end
+    
+    assign z1 = clk & y[3];
+    
+    always@(y or x1) begin
+        case(y)
+            state_a: if(x1==0)
+                        next_state = state_a;
+                     else
+                        next_state = state_b;
+            state_b: if(x1==0)
+                        next_state = state_a;
+                     else
+                        next_state = state_c;
+            state_c: if(x1==0)
+                        next_state = state_d;
+                     else
+                        next_state = state_c;
+            state_d: if(x1==0)
+                        next_state = state_a;
+                     else
+                        next_state = state_e;
+            state_e: if(x1==0)
+                        next_state = state_a;
+                     else
+                        next_state = state_c;
+            default: next_state = state_a;
+        endcase
+    end
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    // All output pins must be assigned. If not used, assign to 0.
+    assign uo_out[0] = y[1];
+    assign uo_out[1] = y[2];
+    assign uo_out[2] = y[3];
+    assign uo_out[3] = z1;
+    assign uo_out[4] = 1'b0;
+    assign uo_out[5] = 1'b0;
+    assign uo_out[6] = 1'b0;
+    assign uo_out[7] = 1'b0;
+
+    assign uio_out = 0;
+    assign uio_oe  = 0;
+
+    // List all unused inputs to prevent warnings
+    wire _unused = &{ena, ui_in[7:1], uio_in, 1'b0};
 
 endmodule
